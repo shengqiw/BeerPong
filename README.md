9f8b08ea5f237cbf1516a5cc1f408ae9655f6763
root <root@DESKTOP-9VB90AA.localdomain>
2018-12-07 04:47:01 -0500

    finished base game

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Beer Pong 
+# AQUA Pong 
 #
 ## Challenge your friends in your favorite party game
 ## It is online multiplayer!
 
-## If you don't know the rules, proceed to the _House Rules_ Page
+## If you don't know the rules, proceed to the _Rules_ Page in game
 
 ### Solo Project by SHENGQI WANG in CS252: Systems Programming 
 ### Email: wang2741@purdue.edu
--- home.js
+++ home.js
@@ -1,3 +1,7 @@
+let clock = 10;
+let timer = document.getElementById("id_timer");
+let SCORE = document.getElementById("id_score");
+
 function animatethis(targetElement) {
     $(targetElement).animate({ "top": "-=150px"},
     {
@@ -15,35 +19,74 @@ function animatethis(targetElement) {
         }
     });
 };
-
-animatethis($('#img_ball'));
+$("#btn_begin").click(function() {
+    animatethis($('#img_ball'));
+    handleBegin();
+});
 
 $("#btn_throw").click(function(){
     var x = $("#img_ball").offset();
     // alert("Top: " + x.top + " Left: " + x.left);
     handleThrow(x.top);
 });
+function handleBegin() {
+    document.getElementById("btn_begin").style.display = "none";
+    document.getElementById("btn_throw").style.display = "block";
+    let elements = document.getElementsByClassName("info");
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "block";
+    }
+    setInterval(decrement, 1000);
+}
+
+function decrement() {
+    clock--;
+    if (clock <= 0) {
+        handleGameOver();
+    }
+    else if (clock == 1)
+        timer.innerHTML = clock + " sec";
+    else
+        timer.innerHTML = clock + " secs";    
+}
+
+function handleReset() {
+    /*document.getElementById("btn_begin").style.display = "block";
+    document.getElementById("btn_throw").style.display = "none";
+    timer.innerHTML = "10 secs";
+    document.getElementById("id_throws").innerHTML = "3";
+    let elements = document.getElementsByClassName("info");
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+    }
+    $("#img_ball").finish();
+    */
+    window.location.href = "home.html";
+}
 
 function handleThrow(pos) {
     let ball = document.getElementById("img_table");
     let num = Math.floor(Math.random() *4) + 1;
-    if (pos < 550) {
+    // /alert(pos);
+    if (pos < 490) {
         ball.src="img/pong_board_miss_" + num + ".png";
         handleScore(0, -1);
     }
-    else if (pos < 575) {
+    else if (pos < 515) {
         ball.src="img/pong_board_1.png";
         handleScore(4, 0);
     }
-    else if (pos < 600) {
+    else if (pos < 540) {
         ball.src="img/pong_board_2.png";
         handleScore(3, 0);
     }
-    else if (pos < 625) {
+    else if (pos < 565) {
         ball.src="img/pong_board_3.png";
         handleScore(2, 0);
     }
-    else if (pos < 650) {
+    else if (pos < 590) {
         ball.src="img/pong_board_4.png";
         handleScore(1, 0);
     }
@@ -54,14 +97,19 @@ function handleThrow(pos) {
 }
 
 function handleScore(score, throws) {
-    let SCORE = document.getElementById("id_score");
     let THROWS = document.getElementById("id_throws"); 
     let throws_left = parseInt(THROWS.innerHTML, 10) + throws;
     SCORE.innerHTML = parseInt(SCORE.innerHTML, 10) + score;
-    if (throws_left < 0)
-        alert("YOU LOOOOSE");
-    else 
-        THROWS.innerHTML = parseInt(THROWS.innerHTML, 10) + throws;
+    
+    if (throws_left <= 0) {
+        setTimeout(handleGameOver, 100);        
+    }
+    
+    THROWS.innerHTML = throws_left;
 
+}
 
+function handleGameOver() {
+    alert("Game Over!\nYou Scored " + SCORE.innerHTML);
+    handleReset();  
 }
